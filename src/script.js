ecf1e5cc84273e9e1eb990f460626568754dc656
Ilya Shurmanov <h82st8@gmail.com>
2020-08-24 12:27:08 +0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,21 @@
+import scss from './styles/style.scss';
+
+import favicon from "./images/favicon.png";
+import logo from "./images/logo.svg";
+import poster from "./images/video.jpg";
+import presentation from "./images/presentation.jpg";
+
+import img1 from "./images/members/Ellipse_193.png";
+import img2 from "./images/members/Ellipse_194.png";
+import img3 from "./images/members/Ellipse_195.png";
+import img4 from "./images/members/Ellipse_196.png";
+import img5 from "./images/members/Ellipse_197.png";
+import img6 from "./images/members/Ellipse_198.png";
+import img7 from "./images/members/Ellipse_199.png";
+import img8 from "./images/members/Ellipse_200.png";
+import img9 from "./images/members/Ellipse_201.png";
+import img10 from "./images/members/Ellipse_202.png";
+import img11 from "./images/members/Ellipse_203.png";
+import img12 from "./images/members/Ellipse_204.png";
+import img13 from "./images/members/Ellipse_205.png";
+import img14 from "./images/members/Ellipse_206.png";
